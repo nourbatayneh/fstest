aa533b08797da52d6a1f303818802651d8b8992f
nourbatayneh <30976653+nourbatayneh@users.noreply.github.com>
2018-09-06 15:50:26 +0300

    filesharetest
    filesharetest

---
+++ fstest.ps1
@@ -0,0 +1,3 @@
+﻿$acctKey = ConvertTo-SecureString -String "g1dcp7XrUtwsKPxpsDG7X/uArvLIR+3XXDVTVq8P+s0Sw/4Klsb5bAWCTaeAwiFfqSU93zUHHYAhmVOhbRO98A==" -AsPlainText -Force
+$credential = New-Object System.Management.Automation.PSCredential -ArgumentList "Azure\safileshare", $acctKey
+New-PSDrive -Name Z -PSProvider FileSystem -Root "\\safileshare.file.core.windows.net\scalesetfs" -Credential $credential -Persist
